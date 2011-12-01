5b3dac111a3f5eb98d367314d6c530380359781a
Tom Crinson <the.jaba@gmail.com>
2011-12-01 09:41:44 +0000

    Added in people spoken to and frequency

--- schlurp.rb
+++ schlurp.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'faraday'
+require 'JSON'
+require 'ruby-debug'
 
 connection = Faraday.new(:url => 'https://schlurp-api.apigee.com/v1/twitter/1/statuses/user_timeline.json?') do |builder|
   builder.use Faraday::Request::UrlEncoded  # convert request params as "www-form-urlencoded"
@@ -11,4 +13,7 @@ response = connection.get do |request|
   request.params['smartkey']='29fb06fd-3935-4439-b896-e861156b4a87'
 end
 
-p response
+timeline = JSON.parse(response.body) 
+spoken_to = Hash.new{|name,frequency| name[frequency]= 0}
+timeline.each{|tweet| tweet['text'].scan(/@[a-z0-9_]+/i).each{|name| spoken_to[name] += 1 } }
+p spoken_to
