db8ff0e0240a856a32132dec1bc2ef7d25c50bff
Tom Crinson <the.jaba@gmail.com>
2011-12-01 20:56:26 +0000

    Added in link extraction, resolving and fetching

--- schlurp.rb
+++ schlurp.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'faraday'
 require 'JSON'
 require 'ruby-debug'
+require 'twitter-text'
 
 connection = Faraday.new(:url => 'https://schlurp-api.apigee.com/v1/twitter/1/statuses/user_timeline.json?') do |builder|
   builder.use Faraday::Request::UrlEncoded  # convert request params as "www-form-urlencoded"
@@ -13,7 +14,30 @@ response = connection.get do |request|
   request.params['smartkey']='29fb06fd-3935-4439-b896-e861156b4a87'
 end
 
+include Twitter::Extractor
+
 timeline = JSON.parse(response.body) 
 spoken_to = Hash.new{|name,frequency| name[frequency]= 0}
-timeline.each{|tweet| tweet['text'].scan(/@[a-z0-9_]+/i).each{|name| spoken_to[name] += 1 } }
+timeline.each{|tweet| extract_mentioned_screen_names(tweet['text']).each{|name| spoken_to[name] += 1 } }
+timeline.each{|t| p t['text']}
+links = timeline.collect{|tweet| extract_urls(tweet['text'])}.flatten
 p spoken_to
+p links
+
+resolver = Faraday.new do |builder|
+  builder.use Faraday::Adapter::NetHttp     # make http requests with Net::HTTP
+end
+  
+resolved = links.collect do |link|
+  response = resolver.get do |req|
+    req.url link
+  end
+  response.headers['location']
+end
+
+resolved.each do |uri|
+  response = resolver.get do |req|
+    req.url uri
+  end
+  p response.body
+end
